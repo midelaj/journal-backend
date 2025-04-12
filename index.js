ae9f0f3b487232483fce81e17716628de8b79dcc
midelaj <i.midelaj@gmail.com>
2025-04-12 13:40:57 +0530

    journal done

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(passport.initialize());
 
 db();
 app.use('/', route)
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT
 app.listen(PORT,()=>{
     console.log(`server running on  ${PORT}`)
 })
